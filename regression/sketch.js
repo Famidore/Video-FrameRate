f6899d367bf3348813098fcf0642495ef90f5f89
Famidore <lokoroko456@gmail.com>
2022-11-15 04:27:48 +0100

    basic template

--- regression/sketch.js
+++ regression/sketch.js
@@ -1,7 +1,33 @@
+let dropzone;
+let vid;
+
 function setup() {
-  createCanvas(400, 400);
+  noCanvas()
+  dropzone = select('#dropzone');
+  dropzone.dragOver(highlight);
+  dropzone.dragLeave(unhighlight)
+  dropzone.drop(readFile, unhighlight)
+  
 }
 
 function draw() {
-  background(220);
+  background(51);
+  // createP(getFrameRate())
+}
+
+
+
+function highlight(){
+  dropzone.style('background-color', '#ccc')
+}
+
+function unhighlight(){
+  dropzone.style('background-color', '#fff')
+}
+
+function readFile(file){
+  vid = createVideo(file.data)
+  vid.size(1200,640)
+  vid.volume(0)
+  vid.play()
 }
